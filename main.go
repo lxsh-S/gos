a6d1aefa5cffaa04d69034f34205b23c73f30432
lxsh-S <kumarlakshya973@gmail.com>
2026-07-19 01:47:15 +0530

    Test CD workflow

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 			}
 			return cobra.ExactArgs(1)(cmd, args)
 		},
-		Version: "0.6.9",
+		Version: "0.7.1",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if list {
 				printSupported()
