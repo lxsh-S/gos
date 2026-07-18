103b3ae3e6ded7fd9469fac2b03f2ba091ea6f2b
lxsh-S <kumarlakshya973@gmail.com>
2026-07-19 01:30:05 +0530

    v0.6.8

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 			}
 			return cobra.ExactArgs(1)(cmd, args)
 		},
-		Version: "0.6.7",
+		Version: "0.6.8",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if list {
 				printSupported()
