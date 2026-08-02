5c00f87eb015ee417a8713be1a5fb7a6f884ef31
lxsh-S <kumarlakshya973@gmail.com>
2026-08-02 14:56:19 +0530

    feat: Init a git repo

--- internal/cli/define/create.go
+++ internal/cli/define/create.go
@@ -2,27 +2,34 @@ package define
 
 import (
 	"fmt"
-	"log"
 	"os/exec"
 
 	"github.com/lxsh-S/gos/internal/cli/create"
 )
 
 func Create(projectName, projectType, projectLang string) error {
-	gitcmd := exec.Command("git", "init")
-	err := gitcmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// Create the project files first
+	var err error
 	switch projectLang {
 	case "go":
-		return create.CreateGO(projectName, projectType)
+		err = create.CreateGO(projectName, projectType)
 	case "ts":
-		return create.CreateTS(projectName, projectType)
+		err = create.CreateTS(projectName, projectType)
 	case "cpp":
-		return create.CreatCPP(projectName, projectType)
+		err = create.CreatCPP(projectName, projectType)
 	default:
 		return fmt.Errorf("unknown project language: %q (expected: go, ts, cpp)", projectLang)
 	}
+
+	if err != nil {
+		return fmt.Errorf("failed to create project: %w", err)
+	}
+
+	// Initialize git inside the project directory
+	cmd := exec.Command("git", "init", projectName)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("failed to initialize git repository in %s: %w\n%s", projectName, err, string(output))
+	}
+
+	return nil
 }
--- internal/cli/define/mkdir.go
+++ internal/cli/define/mkdir.go
@@ -1,9 +1,20 @@
 package define
 
 import (
+	"fmt"
+	"os/exec"
+
 	"github.com/lxsh-S/gos/internal/tools/mkdir"
 )
 
 func GOMkdirRun(folderName string) {
 	mkdir.GOMkdir(folderName)
+	cmd := exec.Command("git", "init", folderName)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("Error initializing git repository in %s: %v\n%s\n", folderName, err, string(output))
+		return
+	}
+
+	fmt.Print(string(output))
 }
